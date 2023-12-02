4cff693f185bf30ae3d53c4c0397e7154f7c2b3c
Julian Nymark <j@juliannymark.com>
2023-12-02 17:13:09 +0100

    :construction: wip on getting rust to compile

--- 2023/aoc_2023_1/src/main.rs
+++ 2023/aoc_2023_1/src/main.rs
@@ -1,7 +1,23 @@
+use regex::Regex;
 use std::fs;
 
 fn main() {
-    let file_contents = fs::read_to_string("input.txt");
+    let file_contents = fs::read_to_string("input.txt").unwrap();
 
-    println!("{}", file_contents.unwrap())
+    let re = Regex::new(r"[0-9]").unwrap();
+
+    for line in file_contents.lines() {
+        let first_match = re.find(line).unwrap();
+        // let reversed_line = reverse(&line);
+        let last_match = re.find(reverse(line)).unwrap();
+        // println!("{line}");
+        println!("first_match: {}", first_match.as_str());
+        println!("last_match: {}", last_match.as_str());
+    }
+
+    // println!("{}", file_contents.unwrap());
+}
+
+fn reverse(s: &str) -> &str {
+    s.chars().rev().collect::<String>().as_str()
 }
