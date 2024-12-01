4a7fc3e934f56d733e08499201fe6461046fd990
Julian Nymark <j@juliannymark.com>
2024-12-01 16:10:45 +0100

    part 2 + refactor part 1 slightly (more borrowing)

--- 2024/day_1/src/main.rs
+++ 2024/day_1/src/main.rs
@@ -6,7 +6,7 @@ mod tests {
 
     #[test]
     fn test_1() {
-        assert_eq!(sad(vec!["1", "2"], vec!["5", "7"]), 9);
+        assert_eq!(sad(&vec!["1", "2"], &vec!["5", "7"]), 9);
     }
 
     #[test]
@@ -21,10 +21,21 @@ mod tests {
     fn test_example() {
         assert_eq!(
             sad(
-                sort_string_nums(vec!["3", "4", "2", "1", "3", "3",]),
-                sort_string_nums(vec!["4", "3", "5", "3", "9", "3",])
+                &sort_string_nums(vec!["3", "4", "2", "1", "3", "3",]),
+                &sort_string_nums(vec!["4", "3", "5", "3", "9", "3",])
             ),
             11
+        )
+    }
+
+    #[test]
+    fn test_3() {
+        assert_eq!(
+            wack_similarity_score_algo(
+                &vec!["3", "4", "2", "1", "3", "3",],
+                &vec!["4", "3", "5", "3", "9", "3",]
+            ),
+            31
         )
     }
 }
@@ -35,11 +46,31 @@ fn sort_string_nums(string_nums: Vec<&str>) -> Vec<&str> {
     copy
 }
 
+fn wack_similarity_score_algo(list1: &Vec<&str>, list2: &Vec<&str>) -> u32 {
+    let mut similarity = 0;
+
+    for (_idx, value) in list1.into_iter().enumerate() {
+        let num1 = value.parse::<i32>().unwrap();
+        let mut count = 0;
+
+        for (_idx2, value2) in list2.into_iter().enumerate() {
+            let num2 = value2.parse::<i32>().unwrap();
+
+            if num1 == num2 {
+                count += 1;
+            }
+        }
+        similarity += count * num1;
+    }
+
+    similarity.try_into().unwrap()
+}
+
 /*
 SAD == Sum of Absolute Differences
 ASSUMPTION: both iterables must be of the same length
 */
-fn sad(list1: Vec<&str>, list2: Vec<&str>) -> u32 {
+fn sad(list1: &Vec<&str>, list2: &Vec<&str>) -> u32 {
     let mut sum = 0;
 
     for (idx, value) in list1.into_iter().enumerate() {
@@ -50,7 +81,7 @@ fn sad(list1: Vec<&str>, list2: Vec<&str>) -> u32 {
     return sum.try_into().unwrap();
 }
 
-fn main() {
+fn part_1() {
     let file_name = "./input.txt";
     let input = fs::read_to_string(file_name)
         .expect(format!("expected to read file {}", file_name).as_str());
@@ -58,17 +89,14 @@ fn main() {
     let mut list_a: Vec<&str> = Vec::new();
     let mut list_b: Vec<&str> = Vec::new();
 
-    // TODO: why does this `.split()` create a final `""` entry?!
     for line in input.split("\n") {
-        // println!("{}", line)
-
         // variant 1: loop
         /*
         for (idx, location_id) in line.split_whitespace().enumerate() {
-            println!("({}) {}", idx, location_id);
-            //     if (idx == 0) {
-            //      ...
-            //     }
+        println!("({}) {}", idx, location_id);
+        //     if (idx == 0) {
+        //      ...
+        //     }
         }
         */
 
@@ -88,7 +116,35 @@ fn main() {
     }
 
     println!(
-        "{}",
-        sad(sort_string_nums(list_a), sort_string_nums(list_b))
+        "part 1: {}",
+        sad(&sort_string_nums(list_a), &sort_string_nums(list_b))
     );
+}
+
+fn part_2() {
+    let file_name = "./input.txt";
+    let input = fs::read_to_string(file_name)
+        .expect(format!("expected to read file {}", file_name).as_str());
+
+    let mut list_a: Vec<&str> = Vec::new();
+    let mut list_b: Vec<&str> = Vec::new();
+
+    for line in input.split("\n") {
+        // WHY: the last entry will be None (from split on last `\n` in file)
+        if line != "" {
+            let mut split_lines_iter = line.split_whitespace();
+            let list_a_location = split_lines_iter.next().unwrap();
+            let list_b_location = split_lines_iter.next().unwrap();
+
+            list_a.push(list_a_location);
+            list_b.push(list_b_location);
+        }
+    }
+
+    println!("part 2: {}", wack_similarity_score_algo(&list_a, &list_b));
+}
+
+fn main() {
+    part_1();
+    part_2();
 }
