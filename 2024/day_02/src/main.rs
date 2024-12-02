c998d26c402f536189455de8d285936b975c91e4
Julian Nymark <j@juliannymark.com>
2024-12-02 23:05:50 +0100

    :star: day 2 part 1

--- 2024/day_02/src/main.rs
+++ 2024/day_02/src/main.rs
@@ -38,7 +38,57 @@ mod tests {
 
 type Report = Vec<i32>;
 
-fn check_valid(_report: Report) -> bool {
+enum Direction {
+    Up,
+    Down,
+}
+
+/// check if a list of numbers follows some rules
+///
+/// assumes a vec of at least 2 numbers.
+fn check_valid(report: Report) -> bool {
+    let dir: Direction;
+
+    if report[0] == report[1] || (report[0] - report[1]).abs() > 3 {
+        return false;
+    }
+
+    if report[1] > report[0] {
+        dir = Direction::Up;
+    } else {
+        dir = Direction::Down;
+    }
+
+    let mut last_seen = report[1];
+
+    // really don't like this way of doing it
+    for &value in &report[2..] {
+        if value > last_seen {
+            match dir {
+                Direction::Up => {
+                    let diff = value - last_seen;
+                    if diff > 3 || diff <= 0 {
+                        return false;
+                    }
+                }
+                _ => return false,
+            }
+        } else if value < last_seen {
+            match dir {
+                Direction::Down => {
+                    let diff = last_seen - value;
+                    if diff > 3 || diff <= 0 {
+                        return false;
+                    }
+                }
+                _ => return false,
+            }
+        } else {
+            return false;
+        }
+        last_seen = value;
+    }
+
     true
 }
 
@@ -72,11 +122,11 @@ fn part_1() {
     println!("part 1: {}", safe_count);
 }
 
-fn part_2() {
+/* fn part_2() {
     todo!();
-}
+} */
 
 fn main() {
     part_1();
-    part_2();
+    // part_2();
 }
