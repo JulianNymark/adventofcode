4f2332c8ac7175ab5d4cb46fd2bda905f4c2deab
Julian Nymark <j@juliannymark.com>
2024-12-03 01:45:19 +0100

    a lot of tests: but non-working part 2

--- 2024/day_02/src/main.rs
+++ 2024/day_02/src/main.rs
@@ -7,93 +7,178 @@ mod tests {
 
     #[test]
     fn test_1() {
-        assert_eq!(check_valid(vec![7, 6, 4, 2, 1]), true)
+        assert_eq!(score_report(vec![7, 6, 4, 2, 1]), 0)
     }
 
     #[test]
     fn test_2() {
-        assert_eq!(check_valid(vec![1, 2, 7, 8, 9]), false)
+        assert_eq!(score_report(vec![1, 2, 7, 8, 9]), 1)
     }
 
     #[test]
     fn test_3() {
-        assert_eq!(check_valid(vec![9, 7, 6, 2, 1]), false)
+        assert_eq!(score_report(vec![9, 7, 6, 2, 1]), 1)
     }
 
     #[test]
     fn test_4() {
-        assert_eq!(check_valid(vec![1, 3, 2, 4, 5]), false)
+        assert_eq!(score_report(vec![1, 3, 2, 4, 5]), 1)
     }
 
     #[test]
     fn test_5() {
-        assert_eq!(check_valid(vec![8, 6, 4, 4, 1]), false)
+        assert_eq!(score_report(vec![8, 6, 4, 4, 1]), 1)
     }
 
     #[test]
     fn test_6() {
-        assert_eq!(check_valid(vec![1, 3, 6, 7, 9]), true)
+        assert_eq!(score_report(vec![1, 3, 6, 7, 9]), 0)
+    }
+
+    #[test]
+    fn test_7() {
+        assert_eq!(score_report(vec![8, 3, 6, 7, 9]), 4)
+    }
+
+    #[test]
+    fn test_8() {
+        assert_eq!(score_report(dampen_report(vec![8, 3, 6, 7, 9])), 2)
+    }
+
+    #[test]
+    fn test_9() {
+        assert_eq!(dampen_report(vec![8, 3, 6, 7, 9]), vec![8, 6, 7, 9])
+    }
+
+    #[test]
+    fn test_10() {
+        assert_eq!(dampen_report(vec![1, 2, 9]), vec![1, 2])
+    }
+
+    #[test]
+    fn test_11() {
+        assert_eq!(dampen_report(vec![1, 2, 3, 2, 1]), vec![1, 2, 2, 1])
+    }
+
+    #[test]
+    fn test_12() {
+        assert_eq!(dampen_report(vec![1, 2, 10, 11, 12]), vec![1, 2, 11, 12])
+    }
+
+    #[test]
+    fn test_13() {
+        assert_eq!(dampen_report(vec![1, 2, 3, 4, 4]), vec![1, 2, 3, 4])
+    }
+
+    #[test]
+    fn test_14() {
+        assert_eq!(dampen_report(vec![1, 1, 2, 3, 4, 4]), vec![1, 2, 3, 4, 4])
+    }
+
+    #[test]
+    fn test_15() {
+        assert_eq!(
+            dampen_report(vec![40, 41, 42, 45, 47, 51, 53, 60]),
+            vec![40, 41, 42, 45, 47, 53, 60]
+        )
+    }
+
+    #[test]
+    fn test_16() {
+        assert_eq!(score_report(vec![72, 72, 74, 72, 75, 78, 80, 86]), 3)
+    }
+
+    #[test]
+    fn test_17() {
+        assert_eq!(
+            score_report(dampen_report(vec![72, 72, 74, 72, 75, 78, 80, 86])),
+            2
+        )
     }
 }
 
 type Report = Vec<i32>;
 
-enum Direction {
-    Up,
-    Down,
+/// will remove the first bad level if found
+fn dampen_report(report: Report) -> Report {
+    let mut dir = 0;
+
+    for (idx, level) in report[..report.len() - 1].iter().enumerate() {
+        let next_level = report[idx + 1];
+
+        let diff = next_level - level;
+
+        let mut skip = false;
+
+        if diff.abs() > 3 || diff == 0 {
+            skip = true;
+        }
+
+        if diff > 0 {
+            if dir == -1 {
+                return [report[..idx].to_vec(), report[idx + 1..].to_vec()].concat();
+            }
+            if dir == 0 {
+                dir = 1
+            }
+        } else if diff < 0 {
+            if dir == 1 {
+                return [report[..idx].to_vec(), report[idx + 1..].to_vec()].concat();
+            }
+            if dir == 0 {
+                dir = -1
+            }
+        }
+        if skip {
+            return [report[..idx + 1].to_vec(), report[idx + 2..].to_vec()].concat();
+        }
+    }
+    return report;
 }
 
 /// check if a list of numbers follows some rules
+/// returns a badness score
 ///
 /// assumes a vec of at least 2 numbers.
-fn check_valid(report: Report) -> bool {
-    let dir: Direction;
+///
+/// 555 ... x ... 565: could check all 10 manually
+fn score_report(report: Report) -> i32 {
+    let mut badnesss = 0;
+    let mut dir = 0;
 
-    if report[0] == report[1] || (report[0] - report[1]).abs() > 3 {
-        return false;
-    }
+    for (idx, level) in report[..report.len() - 1].iter().enumerate() {
+        let next_level = report[idx + 1];
 
-    if report[1] > report[0] {
-        dir = Direction::Up;
-    } else {
-        dir = Direction::Down;
-    }
+        let diff = next_level - level;
 
-    let mut last_seen = report[1];
-
-    // really don't like this way of doing it
-    for &value in &report[2..] {
-        if value > last_seen {
-            match dir {
-                Direction::Up => {
-                    let diff = value - last_seen;
-                    if diff > 3 || diff <= 0 {
-                        return false;
-                    }
-                }
-                _ => return false,
+        if diff > 0 {
+            if dir == -1 {
+                badnesss += 1;
             }
-        } else if value < last_seen {
-            match dir {
-                Direction::Down => {
-                    let diff = last_seen - value;
-                    if diff > 3 || diff <= 0 {
-                        return false;
-                    }
-                }
-                _ => return false,
+            if diff.abs() > 3 {
+                badnesss += 1;
+            }
+            if dir == 0 {
+                dir = 1
+            }
+        } else if diff < 0 {
+            if dir == 1 {
+                badnesss += 1;
+            }
+            if diff.abs() > 3 {
+                badnesss += 1;
+            }
+            if dir == 0 {
+                dir = -1
             }
         } else {
-            return false;
+            badnesss += 1;
         }
-        last_seen = value;
     }
-
-    true
+    return badnesss;
 }
 
-fn part_1() {
-    let file_name = "./input.txt";
+fn get_reports(file_name: &str) -> Vec<Report> {
     let input = fs::read_to_string(file_name)
         .expect(format!("expected to read file {}", file_name).as_str());
 
@@ -112,9 +197,16 @@ fn part_1() {
         }
     }
 
+    reports
+}
+
+fn part_1() {
+    let reports = get_reports("./input.txt");
+
     let mut safe_count = 0;
+
     for report in reports {
-        if check_valid(report) {
+        if score_report(report) == 0 {
             safe_count += 1;
         }
     }
@@ -122,11 +214,21 @@ fn part_1() {
     println!("part 1: {}", safe_count);
 }
 
-/* fn part_2() {
-    todo!();
-} */
+fn part_2() {
+    let reports = get_reports("./input.txt");
+
+    let mut safe_count = 0;
+
+    for report in reports {
+        if score_report(dampen_report(report)) == 0 {
+            safe_count += 1;
+        }
+    }
+
+    println!("part 2: {}", safe_count);
+}
 
 fn main() {
     part_1();
-    // part_2();
+    part_2();
 }
