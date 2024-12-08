93512c699193d655319cfd7fcebf896df7f96b7c
Julian Nymark <j@juliannymark.com>
2024-12-08 20:01:49 +0100

    :bug: fix part_1 that didn't break solution

--- 2024/day_04/src/main.rs
+++ 2024/day_04/src/main.rs
@@ -1,14 +1,27 @@
 use grid::Grid;
 
 fn main() {
+    part_1();
+    part_2();
+}
+
+fn part_1() {
     let lines = utils::lines("./input.txt");
     let x_dim = lines[0].len();
     let _y_dim = lines.len();
 
     let mut grid: Grid<char> = Grid::from_vec(lines.join("").chars().collect(), x_dim);
 
-    let element = grid.get_mut(42, 42);
-    *element.unwrap() = 'b';
+    let count = iter_thru_grid(&grid);
+    println!("part 1: {}", count);
+}
+
+fn part_2() {
+    let lines = utils::lines("./input.txt");
+    let x_dim = lines[0].len();
+    let _y_dim = lines.len();
+
+    let mut grid: Grid<char> = Grid::from_vec(lines.join("").chars().collect(), x_dim);
 
     let count = iter_thru_grid(&grid);
     println!("part 1: {}", count);
