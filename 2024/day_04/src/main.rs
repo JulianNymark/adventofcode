9c3fcf53fce467477583e6360b1a857fce5990a7
Julian Nymark <j@juliannymark.com>
2024-12-08 19:52:36 +0100

    :star: day 4 part 1

--- 2024/day_04/src/main.rs
+++ 2024/day_04/src/main.rs
@@ -10,10 +10,58 @@ fn main() {
     let element = grid.get_mut(42, 42);
     *element.unwrap() = 'b';
 
-    print_grid(&grid);
+    let count = iter_thru_grid(&grid);
+    println!("part 1: {}", count);
 }
 
-fn print_grid(grid: &Grid<char>) {
+fn iter_thru_grid(grid: &Grid<char>) -> i32 {
+    let mut count = 0;
+    for (row_idx, row) in grid.iter_rows().enumerate() {
+        for (col_idx, char) in row.enumerate() {
+            if *char == 'X' {
+                count += sweep_rotate_xmas_check(grid, row_idx, col_idx);
+            }
+        }
+    }
+    count
+}
+
+const DIRECTIONS: [(i32, i32); 8] = [
+    (0, -1),
+    (1, -1),
+    (1, 0),
+    (1, 1),
+    (0, 1),
+    (-1, 1),
+    (-1, 0),
+    (-1, -1),
+];
+
+/// check all 8 directions on grid N, NE, E, SE, S, SW, W, NW
+fn sweep_rotate_xmas_check(grid: &Grid<char>, row_idx: usize, col_idx: usize) -> i32 {
+    let mut count = 0;
+    for direction in DIRECTIONS {
+        let x = TryInto::<i32>::try_into(row_idx).unwrap();
+        let y = TryInto::<i32>::try_into(col_idx).unwrap();
+
+        let sample_m = grid
+            .get(x + (direction.0), y + (direction.1))
+            .unwrap_or(&'\0');
+        let sample_a = grid
+            .get(x + (direction.0 * 2), y + (direction.1 * 2))
+            .unwrap_or(&'\0');
+        let sample_s = grid
+            .get(x + (direction.0 * 3), y + (direction.1 * 3))
+            .unwrap_or(&'\0');
+        if *sample_m == 'M' && *sample_a == 'A' && *sample_s == 'S' {
+            // println!("XMAS found at {},{}", row_idx, col_idx);
+            count += 1;
+        }
+    }
+    count
+}
+
+fn _print_grid(grid: &Grid<char>) {
     for row in grid.iter_rows() {
         println!("{}", row.collect::<String>());
     }
